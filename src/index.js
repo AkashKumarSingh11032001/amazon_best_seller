282c71cf88b6c19c2eb1082dc39b111cf3112dac
Akash <akashkumarsingh11032001@gmail.com>
2023-03-18 19:25:39 +0530

    ./

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Greeting = () => {
+  return <div>React Tutorial</div>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<Greeting />)
