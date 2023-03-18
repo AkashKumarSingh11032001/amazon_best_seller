b99d97f88eb6803468f3e9bdb9335fd62ba62d7b
Akash <akashkumarsingh11032001@gmail.com>
2023-03-18 23:50:10 +0530

    react component

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const Greeting = () => {
-  return <div>React Tutorial</div>;
+  return (
+    <>
+      <div>React Tutorial</div>
+    </>
+  );
 };
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Greeting />)
+root.render(<Greeting />);
