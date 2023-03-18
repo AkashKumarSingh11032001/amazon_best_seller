514db234c4df2eb82df945c338c910c1b68e149e
Akash <akashkumarsingh11032001@gmail.com>
2023-03-19 00:12:45 +0530

    book structure

--- src/index.js
+++ src/index.js
@@ -1,19 +1,35 @@
 import React from 'react'
 import ReactDOM from "react-dom/client";
 
+const Person = () => {
+    return <h1>The Great Akash</h1>
+}
+
+const Title = () => {
+    return <h3>Let the Past Overcome</h3>
+}
+
+const Image = () => {
+    return <h2>Image place holder</h2>
+}
+
 const Book = () => {
   return (
-    <div>Book</div>
+    <>
+    <Person />
+    <Image />
+    <Title />
+    </>
   )
 }
 
 const BookList = () => {
     return (
-      <>
+      <section>
         <Book />
         <Book />
         <Book />
-      </>
+      </section>
     );
 }
 
