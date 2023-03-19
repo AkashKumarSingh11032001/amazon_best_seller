bac09950973bbdb880a9639d60291f6802a81140
Akash <akashkumarsingh11032001@gmail.com>
2023-03-19 15:04:12 +0530

    ./

--- src/index.js
+++ src/index.js
@@ -45,16 +45,19 @@ const bookList = [
     title: "Interesting Facts For Akash Minds",
     person: "Jordan Moore",
     img: "https://m.media-amazon.com/images/I/410llGwMZGL._SX328_BO1,204,203,200_.jpg",
+    id: 1
   },
   {
     title: "Atomic Habits",
     person: "James Clear",
     img: "https://m.media-amazon.com/images/I/419hlQBPe6L._SY346_.jpg",
+    id: 2
   },
   {
     title: "The 5 AM Club",
     person: "Robin Sharma",
     img: "https://m.media-amazon.com/images/I/41MC15v6UIL._SY344_BO1,204,203,200_.jpg",
+    id: 3
   },
 ];
 
@@ -78,7 +81,7 @@ const BookList = () => {
       /> */}
 
       {bookList.map((book)=>{
-        return <Book title={book.title} person={book.person} img={book.img}/>
+        return <Book title={book.title} person={book.person} img={book.img} key={book.id}/>
       })}
     </section>
   );
