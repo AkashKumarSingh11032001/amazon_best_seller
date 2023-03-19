2a88ef5051c498dafc5c08ad9562cfe50443c534
Akash <akashkumarsingh11032001@gmail.com>
2023-03-19 15:28:36 +0530

    add listiner

--- src/index.js
+++ src/index.js
@@ -24,6 +24,24 @@ const Book = (props) => {
   );
 };
 
+const EventExample = () => {
+  const handleFormInput = () => {
+    console.log("You'r typing something on Input box.")
+  }
+  const habdleButtonClick = () => {
+    alert("You just clicked search button.")
+  }
+  return (
+    <section>
+      <form>
+        <h2> Search Book </h2>
+        <input type="text" name="bookProduct" onClick={handleFormInput} style={{ margin: "1rem 0" }} />
+      </form>
+      <button onClick={habdleButtonClick}>Search</button>
+    </section>
+  );
+};
+
 // const firstBook = {
 //   title: "Interesting Facts For Akash Minds",
 //   person: "Jordan Moore",
@@ -45,19 +63,19 @@ const bookList = [
     title: "Interesting Facts For Akash Minds",
     person: "Jordan Moore",
     img: "https://m.media-amazon.com/images/I/410llGwMZGL._SX328_BO1,204,203,200_.jpg",
-    id: 1
+    id: 1,
   },
   {
     title: "Atomic Habits",
     person: "James Clear",
     img: "https://m.media-amazon.com/images/I/419hlQBPe6L._SY346_.jpg",
-    id: 2
+    id: 2,
   },
   {
     title: "The 5 AM Club",
     person: "Robin Sharma",
     img: "https://m.media-amazon.com/images/I/41MC15v6UIL._SY344_BO1,204,203,200_.jpg",
-    id: 3
+    id: 3,
   },
 ];
 
@@ -79,9 +97,17 @@ const BookList = () => {
         person={thirdBook.person}
         img={thirdBook.img}
       /> */}
+      <EventExample />
 
-      {bookList.map((book)=>{
-        return <Book title={book.title} person={book.person} img={book.img} key={book.id}/>
+      {bookList.map((book) => {
+        return (
+          <Book
+            title={book.title}
+            person={book.person}
+            img={book.img}
+            key={book.id}
+          />
+        );
       })}
     </section>
   );
