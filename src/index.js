b86a294bc3f8b541ddafcb9d246d06e304df4f41
Akash <akashkumarsingh11032001@gmail.com>
2023-03-19 10:04:58 +0530

    book page

--- src/index.js
+++ src/index.js
@@ -1,31 +1,37 @@
 import React from 'react'
 import ReactDOM from "react-dom/client";
+import './index.css'
 
 const Person = () => {
-    return <h1>The Great Akash</h1>
+    return <h4>Jordan Moore </h4>;
 }
 
 const Title = () => {
-    return <h3>Let the Past Overcome</h3>
+    return <h2>Interesting Facts For Curious Minds</h2>;
 }
 
 const Image = () => {
-    return <h2>Image place holder</h2>
+    return (
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg"
+        alt="Interesting Facts For Curious Minds"
+      />
+    );
 }
 
 const Book = () => {
   return (
-    <>
+    <article className='book'>
     <Person />
     <Image />
     <Title />
-    </>
+    </article>
   )
 }
 
 const BookList = () => {
     return (
-      <section>
+      <section className='booklist'>
         <Book />
         <Book />
         <Book />
