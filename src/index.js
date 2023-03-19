7d5f79e7117adfc47094121ea0d895c22be564e0
Akash <akashkumarsingh11032001@gmail.com>
2023-03-19 11:20:22 +0530

    book show case

--- src/index.js
+++ src/index.js
@@ -10,10 +10,10 @@ const Title = (props) => {
     return <h2>{props.title}</h2>;
 }
 
-const Image = () => {
+const Image = (props) => {
     return (
       <img
-        src="https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg"
+        src={props.img}
         alt="Interesting Facts For Curious Minds"
       />
     );
@@ -23,25 +23,48 @@ const Book = (props) => {
   
   return (
     <article className='book'>
-    <Image />
+    <Image img={props.img}/>
     <Person person={props.person}/>
     <Title title={props.title}/>
     </article>
   )
 }
 
+const firstBook = {
+  title: "Interesting Facts For Akash Minds",
+  person: "Jordan Moore",
+  img: "https://m.media-amazon.com/images/I/410llGwMZGL._SX328_BO1,204,203,200_.jpg",
+};
+const secondBook = {
+  title: "Atomic Habits",
+  person: "James Clear",
+  img: "https://m.media-amazon.com/images/I/419hlQBPe6L._SY346_.jpg",
+};
+const thirdBook = {
+  title: "The 5 AM Club",
+  person: "Robin Sharma",
+  img: "https://m.media-amazon.com/images/I/41MC15v6UIL._SY344_BO1,204,203,200_.jpg",
+};
+
 const BookList = () => {
-  const title_1 = "Interesting Facts For Akash Minds";
-  const person_1 = "Akash Singh"
-  const title_2 = "Akash Minds";
-  const person_2 = "AKS"
-  const title_3 = "Good Improvement";
-  const person_3 = "Singh Akash";
+  
   return (
     <section className="booklist">
-      <Book title={title_1} person={person_1} />
-      <Book title={title_2} person={person_2} />
-      <Book title={title_3} person={person_3} />
+      <Book
+        title={firstBook.title}
+        person={firstBook.person}
+        img={firstBook.img}
+      />
+      <Book
+        title={secondBook.title}
+        person={secondBook.person}
+        img={secondBook.img}
+      />
+      <Book
+        title={thirdBook.title}
+        person={thirdBook.person}
+        img={thirdBook.img}
+      />
     </section>
   );
 }
